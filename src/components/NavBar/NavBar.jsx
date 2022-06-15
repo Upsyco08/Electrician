82f0bef8575773a56da405f3255eca1b44ec8dcd
Sandesh Raj Shakya <sandsshakya444@gmail.com>
2022-06-15 13:47:30 +0545

    Navbar animations added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import "./index.scss"
-import FlashOnIcon from '@mui/icons-material/FlashOn';
+import lighteningLogo from "../../assets/Lightening.png"
+import BoltIcon from '@mui/icons-material/Bolt';
 
 function NavBar() {
   return (
     <div className='navbar'>
-          <Link className='link' to="/" >
-            <FlashOnIcon className="flash-icon"/>
-            <p>
-              Electrician
-            </p>
+          <Link className='link-logo' to="/" >
+            <div className='nav-logo'>
+              <img src={lighteningLogo} className="lightening-logo" alt="logo" />
+              <div className='box'></div>
+              <h1>
+                Electrician
+              </h1>
+            </div>
           </Link>
           <Link className='link' to="/" >Home</Link>
           <Link className='link' to="/about" >About Us</Link>
@@ -21,7 +25,10 @@ function NavBar() {
           <Link className='link' to="/shop" >Shop</Link>
           <Link className='link' to="/faq" >Faq</Link>
           <Link className='link' to="/contact" >Contact</Link>
-          <Link className='link' to="/appointment" >Appointment</Link>
+          <Link className='link-button' to="/appointment" >
+              <BoltIcon className='link-icon'/>
+              <p>Appointment</p>
+          </Link>
     </div>
   )
 }
